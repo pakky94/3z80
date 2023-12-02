cfe4c17ac55274da52c3abfd9685b83dcd3844b4
Marco Pacchialat <m.pacchialat@gmail.com>
2023-12-02 22:01:46 +0100

    add load & single byte write commands

--- devkit-fw/main.c
+++ devkit-fw/main.c
@@ -6,8 +6,12 @@
 #include "io.h"
 
 void output_shiftregister();
+char load_mem_addr();
 char read_mem_addr();
-void write_256();
+void do_write_256();
+void write_256(char bank, char addr_high, char *data);
+void do_write_byte();
+void write_byte(char bank, char addr_high, char addr_low, char data);
 
 int main() {
     stdio_init_all();
@@ -24,6 +28,10 @@ int main() {
         scanf("%c", &command);
 
         switch (command) {
+            case 'l':
+                load_mem_addr();
+                break;
+
             case 'r':
                 read_mem_addr();
                 break;
@@ -32,8 +40,12 @@ int main() {
                 output_shiftregister();
                 break;
 
+            case 'w':
+                do_write_byte();
+                break;
+
             case 'W':
-                write_256();
+                do_write_256();
                 break;
 
             default:
@@ -77,15 +89,48 @@ char read_mem_addr() {
     return val;
 }
 
-void write_256() {
+char load_mem_addr() {
+    char bank, addr_high, addr_low;
+    scanf("%c", &bank);
+    scanf("%c", &addr_high);
+    scanf("%c", &addr_low);
+
+    set_mem_read(false);
+    set_mem_write(false);
+
+    set_shiftreg_value(((uint)bank << 8) || (uint)addr_high);
+    set_shiftreg_output_enabled(true);
+
+    set_data_pins_dir(GPIO_IN);
+    set_addr_pins_dir(GPIO_OUT);
+    write_addr_pins(addr_low);
+
+    set_mem_read(true);
+
+    sleep_ms(1000);
+
+    set_addr_pins_dir(GPIO_IN);
+    set_shiftreg_output_enabled(false);
+    set_mem_read(false);
+
+    printf("l\n");
+}
+
+void do_write_256() {
     char bank, addr_high;
     scanf("%c", &bank);
     scanf("%c", &addr_high);
-    char* data[256];
+    char data[256];
     for (int i=0; i<256; i++) {
         scanf("%c", &data[i]);
     }
 
+    write_256(bank, addr_high, data);
+
+    printf("a\n");
+}
+
+void write_256(char bank, char addr_high, char *data) {
     set_mem_read(false);
     set_mem_write(false);
 
@@ -97,7 +142,7 @@ void write_256() {
 
     for (int i=0; i<256; i++) {
         write_addr_pins(i);
-        write_data_pins(*data[i]);
+        write_data_pins(data[i]);
         set_mem_write(true);
         set_mem_write(false);
     }
@@ -105,6 +150,37 @@ void write_256() {
     set_data_pins_dir(GPIO_IN);
     set_addr_pins_dir(GPIO_IN);
     set_shiftreg_output_enabled(false);
+}
+
+void do_write_byte() {
+    char bank, addr_high, addr_low, data;
+
+    scanf("%c", &bank);
+    scanf("%c", &addr_high);
+    scanf("%c", &addr_high);
+    scanf("%c", &data);
+
+    write_byte(bank, addr_high, addr_low, data);
 
     printf("a\n");
+}
+
+void write_byte(char bank, char addr_high, char addr_low, char data) {
+    set_mem_read(false);
+    set_mem_write(false);
+
+    set_data_pins_dir(GPIO_OUT);
+    set_addr_pins_dir(GPIO_OUT);
+
+    set_shiftreg_value(((uint)bank << 8) || (uint)addr_high);
+    set_shiftreg_output_enabled(true);
+
+    write_addr_pins(addr_low);
+    write_data_pins(data);
+    set_mem_write(true);
+    set_mem_write(false);
+
+    set_data_pins_dir(GPIO_IN);
+    set_addr_pins_dir(GPIO_IN);
+    set_shiftreg_output_enabled(false);
 }
